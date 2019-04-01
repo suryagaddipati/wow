9d7d5dd607e8bdb3310728a08601e9a9fe363bcd
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-01 12:06:01 -0500

    Add more resources

--- src/main/examples/ec2.scala
+++ src/main/examples/ec2.scala
@@ -1,4 +1,12 @@
 import wow.aws.AWS
 
+val vpc = AWS.vpc(cidrBlock = "10.0.0.0/16")
+val internetGateway = AWS.internetGateway(vpc = vpc)
+val route = AWS.route(vpc = vpc, internetGateway = internetGateway, destinationCidrBlock = "0.0.0.0/0")
+
+val awsSubnet = AWS.subnet(vpc = vpc, cidrBlock = "10.0.1.0/24", mapPublicIdOnLaunch = true)
+
 val bl = AWS.ec2(instanceType = "t2.micro", ami = "ami-2757f631")
 AWS.eip(instance = bl)
+
+
--- src/main/scala/wow/aws/AWS.scala
+++ src/main/scala/wow/aws/AWS.scala
@@ -5,17 +5,24 @@ import wow._
 import scala.collection.mutable
 
 object AWS extends Provider {
-  def create: Any = plan.create()
-
-  def plan: Plan = Plan(resources)
+  def subnet(vpc: VPC, cidrBlock: String, mapPublicIdOnLaunch: Boolean) = add(Subnet(vpc, cidrBlock, mapPublicIdOnLaunch))
 
 
-  var resources = new mutable.MutableList[Resource]()
-
+  def vpc(cidrBlock: String): VPC = VPC(cidrBlock)
 
   def ec2(ami: String, instanceType: String): Instance = add(Instance(ami, instanceType))
 
   def eip(instance: Instance) = add(EIP(instance))
+
+  def internetGateway(vpc: VPC): InternetGateway = add(InternetGateway(vpc))
+
+  def route(vpc: VPC, internetGateway: InternetGateway, destinationCidrBlock: String): Route = add(Route(vpc, internetGateway, destinationCidrBlock))
+
+  var resources = new mutable.MutableList[Resource]()
+
+  def create: Any = plan.create()
+
+  def plan: Plan = Plan(resources)
 
   def add[R <: Resource](r: R): R = {
     resources += r
@@ -28,7 +35,6 @@ object AWS extends Provider {
       case i: Instance => AWSApi.create(i)
       case eip: EIP => AWSApi.create(eip)
     }
-    // AWSApi.create(resource)
   }
 
 }
@@ -36,6 +42,24 @@ object AWS extends Provider {
 case class Instance(ami: String, instanceType: String) extends Resource {
   override def dependencies: List[Resource] = List()
 
+}
+
+case class VPC(cidrBlock: String) extends Resource {
+  override def dependencies: List[Resource] = List()
+
+}
+
+case class InternetGateway(vpc: VPC) extends Resource {
+  override def dependencies: List[Resource] = List(vpc)
+
+}
+
+case class Route(vpc: VPC, internetGateway: InternetGateway, destinationCidrBlock: String) extends Resource {
+  override def dependencies: List[Resource] = List(vpc, internetGateway)
+}
+
+case class Subnet(vpc: VPC, cidrBlock: String, mapPublicIdOnLaunch: Boolean) extends Resource {
+  override def dependencies: List[Resource] = List(vpc)
 }
 
 case class EIP(instance: Instance) extends Resource {
