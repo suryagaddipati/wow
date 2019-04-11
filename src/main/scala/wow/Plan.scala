4e2091f732cab56ec6388dd3877fcbced788186d
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-11 11:08:11 -0500

    Skip if exists

--- src/main/scala/wow/Plan.scala
+++ src/main/scala/wow/Plan.scala
@@ -10,21 +10,43 @@ case class Plan(resources: mutable.MutableList[Resource]) {
 
   implicit val awsProvider = AWS
 
-  def create() = roots.foreach({ r =>
-    try {
-      val k = JsonUtil.fromJson[State](readState())
-    } catch {
-      case e => e.printStackTrace()
+
+  def saveState(state: State) = writeState(JsonUtil.toJson(state))
+
+  def create() = saveState(roots.foldLeft(currentState) { (s, r) =>
+    if (s.has(r)) {
+      println(s"Resource ${r} exists. Skipping..")
+      s
+    } else {
+      val resp = r.create
+      s :+ r
     }
-    val resp = r.create
-    val state = State(List(r))
-    writeState(JsonUtil.toJson(state))
   })
 
+  def currentState() = {
+    readState() match {
+      case Some(s) => s
+      case None => State(List())
+    }
+  }
+
+  import java.nio.file.{Paths, Files}
 
   def readState() = {
-    val source = scala.io.Source.fromFile("state.json")
-    try source.mkString finally source.close()
+    if (Files.exists(Paths.get("state.json"))) {
+      val source = scala.io.Source.fromFile("state.json")
+      val state = try source.mkString finally source.close()
+      try {
+        Some(JsonUtil.fromJson[State](state))
+      } catch {
+        case e => {
+          e.printStackTrace();
+          None
+        }
+      }
+    } else {
+      None
+    }
   }
 
   def writeState(state: String) = {
--- src/main/scala/wow/State.scala
+++ src/main/scala/wow/State.scala
@@ -1,3 +1,8 @@
 package wow
 
-case class State(resources: List[Resource])
+case class State(resources: List[Resource]) {
+  def :+(r: Resource): State = State(resources :+ r)
+
+  def has(r: Resource): Boolean = resources.contains(r)
+
+}
