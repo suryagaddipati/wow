4b17a9daa36a7efe199cd49b660562219a12d5f2
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-11 11:38:31 -0500

    Remove comment

--- src/main/scala/wow/Resource.scala
+++ src/main/scala/wow/Resource.scala
@@ -1,14 +1,7 @@
 package wow
 
-import scala.Array
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
-
-//@JsonTypeInfo ( use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-//@JsonSubTypes (Array (
-//new Type (value = classOf[ProductDetailsSimple], name = "simple"),
-//new Type (value = classOf[ProductDetailsComplex], name = "complex")
-//) )
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(Array(
