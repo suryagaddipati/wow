98f85aff63ac5803604283b092d430318e5d6c4d
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-11 11:15:37 -0500

    refactor

--- src/main/scala/wow/Plan.scala
+++ src/main/scala/wow/Plan.scala
@@ -11,9 +11,7 @@ case class Plan(resources: mutable.MutableList[Resource]) {
   implicit val awsProvider = AWS
 
 
-  def saveState(state: State) = writeState(JsonUtil.toJson(state))
-
-  def create() = saveState(roots.foldLeft(currentState) { (s, r) =>
+  def create() = roots.foldLeft(State.current) { (s, r) =>
     if (s.has(r)) {
       println(s"Resource ${r} exists. Skipping..")
       s
@@ -21,41 +19,8 @@ case class Plan(resources: mutable.MutableList[Resource]) {
       val resp = r.create
       s :+ r
     }
-  })
+  }.save()
 
-  def currentState() = {
-    readState() match {
-      case Some(s) => s
-      case None => State(List())
-    }
-  }
-
-  import java.nio.file.{Paths, Files}
-
-  def readState() = {
-    if (Files.exists(Paths.get("state.json"))) {
-      val source = scala.io.Source.fromFile("state.json")
-      val state = try source.mkString finally source.close()
-      try {
-        Some(JsonUtil.fromJson[State](state))
-      } catch {
-        case e => {
-          e.printStackTrace();
-          None
-        }
-      }
-    } else {
-      None
-    }
-  }
-
-  def writeState(state: String) = {
-    import java.io.PrintWriter
-    new PrintWriter("state.json") {
-      write(state);
-      close
-    }
-  }
 
   def roots: List[Resource] = {
     var rList = resources.toList
--- src/main/scala/wow/State.scala
+++ src/main/scala/wow/State.scala
@@ -5,4 +5,43 @@ case class State(resources: List[Resource]) {
 
   def has(r: Resource): Boolean = resources.contains(r)
 
+  def save() = writeState(JsonUtil.toJson(this))
+
+  def writeState(state: String) = {
+    import java.io.PrintWriter
+    new PrintWriter("state.json") {
+      write(state);
+      close
+    }
+  }
+
+
+}
+
+object State {
+  def current() = {
+    readState() match {
+      case Some(s) => s
+      case None => State(List())
+    }
+  }
+
+  import java.nio.file.{Paths, Files}
+
+  def readState() = {
+    if (Files.exists(Paths.get("state.json"))) {
+      val source = scala.io.Source.fromFile("state.json")
+      val state = try source.mkString finally source.close()
+      try {
+        Some(JsonUtil.fromJson[State](state))
+      } catch {
+        case e => {
+          e.printStackTrace();
+          None
+        }
+      }
+    } else {
+      None
+    }
+  }
 }
