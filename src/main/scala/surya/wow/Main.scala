d0eee9c25f0c3ef6918f0b919a9b205aaa4189d3
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-17 12:46:50 -0500

    command

--- src/main/scala/surya/wow/Main.scala
+++ src/main/scala/surya/wow/Main.scala
@@ -8,8 +8,10 @@ import java.io.{File, PrintWriter}
 
 import surya.wow.aws.AWS
 import xsbti.Exit
+import sbt._
 
 final class Main extends xsbti.AppMain {
+
   //  def main(args: Array[String]) {
   //    val toolbox = currentMirror.mkToolBox()
   //
@@ -22,33 +24,50 @@ final class Main extends xsbti.AppMain {
   //  }
 
 
-  def run(configuration: xsbti.AppConfiguration) = {
-    // get the version of Scala used to launch the application
-    val scalaVersion = configuration.provider.scalaProvider.version
-
-    // Print a message and the arguments to the application
-    println("Hello world!  Running Scala " + scalaVersion)
-    configuration.arguments.foreach(println)
-
-    // demonstrate the ability to reboot the application into different versions of Scala
-    // and how to return the code to exit with
-    scalaVersion match {
-      case "2.10.6" =>
-        new xsbti.Reboot {
-          def arguments = configuration.arguments
-
-          def baseDirectory = configuration.baseDirectory
-
-          def scalaVersion = "2.11.8"
-
-          def app = configuration.provider.id
-        }
-      case "2.11.8" => new Exit(1)
-      case _ => new Exit(0)
-    }
-  }
+  //  def run(configuration: xsbti.AppConfiguration) = {
+  //    // get the version of Scala used to launch the application
+  //    val scalaVersion = configuration.provider.scalaProvider.version
+  //
+  //    // Print a message and the arguments to the application
+  //    println("Hello world!  Running Scala " + scalaVersion)
+  //    configuration.arguments.foreach(println)
+  //
+  //    // demonstrate the ability to reboot the application into different versions of Scala
+  //    // and how to return the code to exit with
+  //    scalaVersion match {
+  //      case "2.10.6" =>
+  //        new xsbti.Reboot {
+  //          def arguments = configuration.arguments
+  //
+  //          def baseDirectory = configuration.baseDirectory
+  //
+  //          def scalaVersion = "2.11.8"
+  //
+  //          def app = configuration.provider.id
+  //        }
+  //      case "2.11.8" => new Exit(1)
+  //      case _ => new Exit(0)
+  //    }
+  //  }
 
   class Exit(val code: Int) extends xsbti.Exit
 
+  def run(configuration: xsbti.AppConfiguration): xsbti.MainResult =
+    MainLoop.runLogged(initialState(configuration))
 
+  def initialState(configuration: xsbti.AppConfiguration): sbt.State = {
+    val commandDefinitions = hello +: BasicCommands.allBasicCommands
+    val commandsToRun = Hello +: "iflast shell" +: configuration.arguments.map(_.trim)
+    sbt.State(configuration, commandDefinitions, Set.empty, None, commandsToRun, sbt.State.newHistory,
+      AttributeMap.empty, initialGlobalLogging, sbt.State.Continue)
+  }
+
+  val Hello = "hello"
+  val hello = Command.command(Hello) { s =>
+    s.log.info("Hello!")
+    s
+  }
+
+  def initialGlobalLogging: GlobalLogging =
+    GlobalLogging.initial(MainLogging.globalDefault(ConsoleOut.systemOut), File.createTempFile("hello", "log"), ConsoleOut.systemOut)
 }
