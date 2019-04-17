63f1e03613d664988f9a68770bfa223dbdb2889d
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-17 13:31:54 -0500

    loop

--- src/main/scala/surya/wow/Main.scala
+++ src/main/scala/surya/wow/Main.scala
@@ -1,9 +1,8 @@
 package surya.wow
 
 
-import java.io.{File, PrintWriter}
+import java.io.File
 
-import xsbti.Exit
 import sbt._
 
 final class Main extends xsbti.AppMain {
@@ -11,30 +10,12 @@ final class Main extends xsbti.AppMain {
   class Exit(val code: Int) extends xsbti.Exit
 
   def run(configuration: xsbti.AppConfiguration): xsbti.MainResult = {
-    val scalaVersion = configuration.provider.scalaProvider.version
-
-    // demonstrate the ability to reboot the application into different versions of Scala
-    // and how to return the code to exit with
-    scalaVersion match {
-      case "2.10.6" =>
-        new xsbti.Reboot {
-          def arguments = configuration.arguments
-
-          def baseDirectory = configuration.baseDirectory
-
-          def scalaVersion = "2.11.8"
-
-          def app = configuration.provider.id
-        }
-      case "2.11.8" => new Exit(1)
-      case _ => new Exit(0)
-    }
     MainLoop.runLogged(initialState(configuration))
   }
 
   def initialState(configuration: xsbti.AppConfiguration): sbt.State = {
     val commandDefinitions = hello +: BasicCommands.allBasicCommands
-    val commandsToRun = Hello +: "iflast shell" +: configuration.arguments.map(_.trim)
+    val commandsToRun = "iflast shell" +: configuration.arguments.map(_.trim)
     sbt.State(configuration, commandDefinitions, Set.empty, None, commandsToRun, sbt.State.newHistory,
       AttributeMap.empty, initialGlobalLogging, sbt.State.Continue)
   }
